e380f9b3c240a938b6f4cde274c0edd558e953f6
WellKochi <zgqqi@126.com>
2017-05-28 15:55:46 -0400

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var index = 0;
 
-var picList = ["images/introscreen.png", "images/1.png", "images/2.png", "images/3.png", "images/4.png", "images/5.png", "images/6.png", "images/7.png", "images/8.png"];
+var picList = ["images/intro.png", "images/1.png", "images/2.png", "images/3.png", "images/4.png", "images/5.png", "images/6.png", "images/7.png", "images/8.png"];
 
 $(function () {
 
